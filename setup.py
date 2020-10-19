62783d8b3ba2d79d76658c2bfc509802374db293
Peter Boers <peter.boers@surfnet.nl>
2020-10-19 15:54:21 +0200

    Added bumpversion and updated setup.cfg

--- setup.py
+++ setup.py
@@ -16,20 +16,27 @@ with open(path.join(here, "requirements.txt"), encoding="utf-8") as f:
 install_requires = [x.strip() for x in all_reqs if "git+" not in x]
 dependency_links = [x.strip().replace("git+", "") for x in all_reqs if "git+" not in x]
 
+with open(path.join(here, "VERSION"), encoding="utf-8") as f:
+    version = f.read()
+
 setup(
     name="pynso-restconf",
     version="2.0.1",
     description="A Python client library for Cisco NSO (previously tail-f)",
     long_description=long_description,
-    url="https://github.com/workfloworchestrator/pynso-restconf",
-    # download_url="https://github.com/tonybaloney/DimensionDataCBUSydney/tarball/" + __version__,
+    url="http://workfloworchestrator.org/pynso-restconf/",
+    download_url="https://github.com/workfloworchestrator/pynso-restconf/archive/" + version + ".tar.gz",
     license="APACHE2",
+    keywords=["NSO", "TAIL-f", "CISCO", "YANG", "RESTCONF", "NETCONF", "CLIENT"],
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Intended Audience :: Developers",
-        "Programming Language :: Python :: 3",
+        'License :: OSI Approved :: Apache2 License',
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
-    keywords="",
     packages=find_packages(exclude=["docs", "tests*"]),
     include_package_data=True,
     author="SURF",
