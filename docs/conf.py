85ae15e1fbbd01f945fc67fcb76e57764bc5eaef
Peter Boers <peter.boers@surfnet.nl>
2020-10-19 16:28:22 +0200

    Isort skip doc file

--- docs/conf.py
+++ docs/conf.py
@@ -12,16 +12,19 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
+'''
+   isort:skip_file
 
+'''
 import os
 import sys
 
-import pynso
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.abspath(".."))
+import pynso
 
 
 # -- General configuration ------------------------------------------------
